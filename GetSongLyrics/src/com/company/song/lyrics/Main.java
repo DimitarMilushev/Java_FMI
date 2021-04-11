1f331f44a5053e9a6423c22928259495ccbae2b5
Dimitar Milushev <DeadlyCookye@gmail.com>
2021-04-11 18:53:48 +0300

    .

--- GetSongLyrics/src/com/company/song/lyrics/Main.java
+++ GetSongLyrics/src/com/company/song/lyrics/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            System.out.println(new GetSongLyrics().getLyrics("Eminem", "Beautiful", false));
+            System.out.println(new GetSongLyrics().getLyrics("Eminem", "Beautiful"));
 
         } catch (URISyntaxException | InterruptedException | IOException e) {
             e.printStackTrace();
