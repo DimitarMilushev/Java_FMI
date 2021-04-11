c1b2c10d3627060080946ac769ece3fe0ff93f59
Dimitar <DeadlyCookye@gmail.com>
2021-04-11 20:06:52 +0300

    .

--- GetSongLyrics/src/com/company/song/lyrics/Main.java
+++ GetSongLyrics/src/com/company/song/lyrics/Main.java
@@ -2,14 +2,20 @@ package com.company.song.lyrics;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String artistName = scanner.next();
+        String songName = scanner.next();
         try {
-            System.out.println(new GetSongLyrics().getLyrics("Eminem", "Beautiful"));
+            System.out.println(new GetSongLyrics().getLyrics(artistName, songName));
 
         } catch (URISyntaxException | InterruptedException | IOException e) {
+            System.out.println(e.getMessage());
             e.printStackTrace();
         }
     }
